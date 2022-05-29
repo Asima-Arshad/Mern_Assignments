14339458fd8717d76f44f24c8279d6f16266c516
Asima-Arshad <asimaarshad10@gmail.com>
2022-05-29 12:30:25 -0700

    asssignment submitted

---
+++ assign_1/app.js
@@ -0,0 +1,9 @@
+// Assignment # 1
+// Make an array of cars and print thier names.
+
+const arr = ["Honda", "volvo" , "BMW", "lamborghini", "Saab"]
+
+arr.forEach (item => { 
+console.log(item);
+
+});
