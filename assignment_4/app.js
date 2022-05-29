ba842e497736757f60aafa2f9e47c95f37ce33be
Asima-Arshad <asimaarshad10@gmail.com>
2022-05-29 13:08:56 -0700

    assignment submitted

---
+++ assignment_4/app.js
@@ -0,0 +1,36 @@
+// Q#1 Write code to sum an array containing numbers.
+
+//sum of array elements using reduce.
+const arr = [12 , 34 , 56 , 76 , 87 , 23 , 66];
+ const sum = arr.reduce((total,value) => total + value , 0);
+ console.log("The sum of the given numbers is " + sum);
+
+//Q#2 Write code to find maximum value from an array of numbers (first using for loop and then using reduce)
+
+// using for loop
+ function findMax(arr) {
+    let max = 0;
+
+// Traverse array elements from second and compare every element with current max. 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+     
+  return max;
+}
+console.log("Using for loop, largest number in given array is " + findMax(arr));
+
+//find max using reduce method
+const output= arr.reduce(function(max, current){
+    if (current > max) {
+        max = current;        
+    }
+    return max;
+}, 0);
+
+console.log("Using reduce, max number in given array is " + output);
+
+
+
+ 
