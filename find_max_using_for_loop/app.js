2ac0663d119180be7cb4440b6fb9b209397794a2
Asima-Arshad <asimaarshad10@gmail.com>
2022-05-29 13:22:15 -0700

    assignment submitted

--- find_max_using_for_loop/app.js
+++ find_max_using_for_loop/app.js
@@ -21,15 +21,7 @@ const arr = [12 , 34 , 56 , 76 , 87 , 23 , 66];
 }
 console.log("Using for loop, largest number in given array is " + findMax(arr));
 
-//find max using reduce method
-const output= arr.reduce(function(max, current){
-    if (current > max) {
-        max = current;        
-    }
-    return max;
-}, 0);
 
-console.log("Using reduce, max number in given array is " + output);
 
 
 
